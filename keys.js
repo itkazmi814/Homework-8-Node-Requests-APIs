16d7ce5b26c97ab55fc960491a7aab2c5fd3b6b5
itkazmi814 <itkazmi814@gmail.com>
2017-10-12 17:44:35 -0500

    pulling from spotify. starting on listing ALL artists

--- keys.js
+++ keys.js
@@ -1,14 +1,21 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: 'ZiEU6YF8y2aME3g9282m4Iurv',
-  consumer_secret: 'cm94y8j5PS52Kk06Gjs8lBUQKdrIdfdP5KNincmamMhIoQN47i',
-  access_token_key: '918555037915385857-rQmAxw9OItdyjyixfvREHmP7JfuolsX',
-  access_token_secret: 'fr6H0YFD7zYMS7YeJr3N0gOde8PJoLbrNyXWr3QyuKcJm'
+	consumer_key: 'ZiEU6YF8y2aME3g9282m4Iurv',
+	consumer_secret: 'cm94y8j5PS52Kk06Gjs8lBUQKdrIdfdP5KNincmamMhIoQN47i',
+	access_token_key: '918555037915385857-rQmAxw9OItdyjyixfvREHmP7JfuolsX',
+	access_token_secret: 'fr6H0YFD7zYMS7YeJr3N0gOde8PJoLbrNyXWr3QyuKcJm'
+}
+
+var spotifyKeys = {
+	id: "dbdd55cc1364494dab6639872a408179",
+	secret: "47a518c16edb4732868af47a3c0eca94"
 }
 
 var keys = {
-	twitterKeys
+	twitterKeys,
+	spotifyKeys
 }
 
 module.exports = keys;
+
--- liri.js
+++ liri.js
@@ -6,26 +6,69 @@ switch(command){
 		twitter();
 		break;
 	case "spotify-this-song":
-		spotify();
+		spotify(process.argv[3]);
 		break;
+	default:
+		console.log("Enter a proper command")
 }
 
 function twitter (){
+	//Calls Twitter node package
 	var Twitter = require('twitter');
+
+	//Passes twitterKeys into the Twitter NPM (?)
 	var client = new Twitter(keys.twitterKeys)
-	var params = {screen_name: 'NUCBimrankazmi', limit: 3};
+	
+	//Creates object is parameters used for the twitter search
+	var params = {
+		screen_name: 'NUCBimrankazmi', 
+		limit: 3
+	};
 
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if (!error) {
 			tweets.forEach(function(i){
-				console.log("Tweet text: " + i.text);
-				console.log("Created at: " + i.created_at);
-				console.log("----")
+				console.log(`${i.text}. Created at: ${i.created_at} by user ${i.user.screen_name}`);
 			})
 		}
 	})
 }
 
-function spotify (){
-	console.log("entering spotify");
-}
+function spotify (songName){
+	var Spotify = require('node-spotify-api');
+	var client = new Spotify(keys.spotifyKeys);
+
+	if(songName === undefined){
+		songName = "The Sign Ace of Base"
+	}
+
+	songName = "Despacito";
+
+	//Establish logic to allow query to have spaces for purposes of process.argv[3]
+	
+	//if no song name given, default to "The Sign" by Ace of Base
+	
+	//since there are multiple artists to a song - create a for loop that iterates through artists
+	//append artists[i].name to a new array
+	//print that array as a String and return
+	 
+	client.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+
+		var artists;
+		var artistsStr;
+
+
+		console.log(data.tracks.items[0].album.artists[0])
+
+		// var dataPath = data.tracks.items[0]
+
+		// console.log(`Song: ${dataPath.name}`)
+		// console.log(`Artist: ${dataPath.artists[0].name}`)
+		// console.log(`Album: ${dataPath.album.name}`)
+		// console.log(`Preview: ${dataPath.preview_url}`)
+
+ 	})
+};
