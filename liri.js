13eeba1da6b0c2677d7ef1f128cfc54883e9304a
itkazmi814 <itkazmi814@gmail.com>
2017-10-12 22:15:52 -0500

    finished spotify, omdb. fixing doIt

--- liri.js
+++ liri.js
@@ -1,17 +1,6 @@
 var keys = require("./keys.js");
 var command = process.argv[2];
 
-switch(command){
-	case "my-tweets":
-		twitter();
-		break;
-	case "spotify-this-song":
-		spotify();
-		break;
-	default:
-		console.log("Enter a proper command")
-}
-
 function setSongName () {
 	var name = ""
 	//Sets song if no value is entered
@@ -37,7 +26,7 @@ function setArtistName (data) {
 		for(var i=1; i<artists.length; i++)
 			string += `, ${artists[i].name}`;
 	}
-	
+
 	return string;
 }
 
@@ -49,21 +38,47 @@ function displaySpotifyData (data,artists) {
 	console.log(`Preview: ${dataPath.preview_url}`)
 }
 
-function twitter (){
+function setMovieName () {
+	var name = ""
+
+	//Sets movie if no value is entered
+	if(process.argv[3] === undefined){
+		name = "Mr. Nobody";
+	}else{
+		//Appends words of the song together into one string
+		for(var i=3; i<process.argv.length;i++)
+			name += process.argv[i] + " "
+	}
+
+	return name;
+}
+
+function displayMovieData (body) {
+	var pBody = JSON.parse(body)
+
+	console.log(`Title: ${pBody.Title}`)
+	console.log(`Year: ${pBody.Year}`)
+	console.log(`IMDB Rating: ${pBody.imdbRating}`)
+	console.log(`RT Rating: ${pBody.Ratings[1].Value}`)
+	console.log(`Country: ${pBody.Country}`)
+	console.log(`Language: ${pBody.Language}`)
+	console.log(`Plot: ${pBody.Plot}`)
+	console.log(`Actors: ${pBody.Actors}`)
+}
+
+function twitter () {
 	//Calls Twitter node package
 	var Twitter = require('twitter');
-
 	//Passes twitterKeys into the Twitter NPM (?)
-	var client = new Twitter(keys.twitterKeys)
-	
+	var client = new Twitter(keys.twitterKeys)	
 	//Creates object is parameters used for the twitter search
 	var params = {
 		screen_name: 'NUCBimrankazmi', 
 		limit: 3
 	};
 
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-		if (!error) {
+	client.get('statuses/user_timeline', params, function(err, tweets, response) {
+		if (!err) {
 			tweets.forEach(function(i){
 				console.log(`${i.text}. Created at: ${i.created_at} by user ${i.user.screen_name}`);
 			})
@@ -71,7 +86,7 @@ function twitter (){
 	})
 }
 
-function spotify (){
+function spotify () {
 	var Spotify = require('node-spotify-api');
 	var client = new Spotify(keys.spotifyKeys);
 	//Validation of entered song name. Allows multiple words
@@ -87,5 +102,65 @@ function spotify (){
 		//Displays song information to the console
 		displaySpotifyData(data,artists);
  	})
-};
+}
 
+function omdb () {
+	var request = require("request")
+
+	var movieName = setMovieName();
+
+	var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+	console.log(queryURL);
+
+	request(queryURL, function(err,response,body){
+		if (!err && response.statusCode === 200) {
+			displayMovieData(body)
+		}
+	})
+}
+
+function doIt () {
+	var fs = require("fs")
+	fs.readFile("random.txt","utf8",function(err,data){
+		if(err){
+			return console.log(err)
+		}
+
+		var dataArr = data.split(",");
+		command = dataArr
+		process.argv[3] = dataArr[2]
+
+		runCommand(command);
+
+	})
+
+	//Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+	//It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+	//Feel free to change the text in that document to test out the feature for other commands.
+}
+
+function runCommand (cmd) {
+
+	switch(cmd){
+		case "my-tweets":
+			twitter();
+			break;
+		case "spotify-this-song":
+			spotify();
+			break;
+
+		case "movie-this":
+			omdb();
+			break;
+
+		case "do-what-it-says":
+			doIt();
+			break;
+
+		default:
+			console.log("Enter a proper command")
+	}
+}
+
+runCommand();
